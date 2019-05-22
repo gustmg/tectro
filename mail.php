263f3f25efaa70e3e4af70007fc3a1cba0bc38a3
gustmg <gustavo.mitre.gallardo@gmail.com>
2019-05-22 16:47:01 -0500

    Feat: Navbar menu

--- mail.php
+++ mail.php
@@ -1,29 +1,34 @@
 <?php
+	$headers = "MIME-Version: 1.0\r\n"; 
+	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+	$headers .= "From: Tectro <ventas@tectro.com.mx>\r\n";
 	$nombre=$_POST['name'];
 	$email=$_POST['email'];
 	$telefono=$_POST['phone'];
 	$mensaje=$_POST['message'];
-	$asunto='Contacto a través de página web';
+	
+	// $nombre="Nombre de prueba";
+	// $email="mail@mail.com";
+	// $telefono="2222222222";
+	// $mensaje="Mensaje de prueba";
+	$asunto="Contacto web";
 
 	$cuerpo=" 
 			<html> 
-			<head> 
-			   <title>Un prospecto ha enviado un correo de contacto.</title> 
+			<head>
+				<meta charset='UTF-8'> 
+			    <title>Un prospecto ha enviado un correo de contacto.</title> 
 			</head> 
 			<body> 
 				<h4>Nombre: </h4>".$nombre."
 				<h4>Correo: </h4>".$email."
-				<h4>Teléfono: </h4>".$telefono."
+				<h4>Telefono: </h4>".$telefono."
 				<h4>Mensaje: </h4>".$mensaje."
 			</body> 
 			</html> 
 			";
 
-	$headers = "MIME-Version: 1.0\r\n"; 
-	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-	$headers .= "From: Tectro <ventas@tectro.com.mx>\r\n";
-
-	if(mail("tectrotecnologico@gmail.com", $asunto, $cuerpo, $headers)){
+	if(mail("ventas@tectro.com.mx", $asunto, $cuerpo, $headers)){
 		echo 1;
 	}
 	else{
